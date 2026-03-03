40639f5a5e18af5f69a239a6ebd5fc155f4576b2
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-03-03 18:56:45 -0300

    Removed Comments

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -31,27 +31,5 @@ public class ConsultaChatGPT {
                 .message()
                 .content()
                 .get();
-
     }
 }
-
-
-
-
-//public class ConsultaChatGPT {
-//    public static String obterTraducao(String texto) {
-//        OpenAiService service = new OpenAiService("OPENAI_API_KEY");
-//
-//        CompletionRequest requisicao = CompletionRequest.builder()
-//                .model("text-davinci-003")
-//                .prompt("traduza para o português o texto: " + texto)
-//                .maxTokens(1000)
-//                .temperature(0.7)
-//                .build();
-//
-//
-//        var resposta = service.createCompletion(requisicao);
-//        return resposta.getChoices().get(0).getText();
-//    }
-//}
-
